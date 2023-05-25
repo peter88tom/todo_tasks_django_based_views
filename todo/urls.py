6c03b4e45ccd03b705c19aa289f8d28ba0b6dacc
petro_thomas <peter88tom@gmail.com>
2023-05-25 10:08:53 +0200

    Create and update task

---
+++ todo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.TaskList.as_view(), name="tasks"),
+    path("<str:pk>", views.TaskDetail.as_view(), name="task"),
+    path("<str:pk>/delete", views.DeleteTask.as_view(), name="delete"),
+]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from django.views import View
 
-# Create your views here.
+
+class TaskList(View):
+    """
+        Create and get tasks items
+    """
+
+    def get(self, request):
+        tasks = Task.objects.all().order_by("-updated")
+        context = {"tasks": tasks}
+        return render(request, "todo/index.html", context)
+
+
+    def post(self, request):
+        task = Task.objects.create(
+            body=request.POST.get("body")
+        )
+        task.save()
+
+        return redirect('tasks')
+
+
+
+class TaskDetail(View):
+    """
+        View a single task and update single task.
+    """
+
+    def get(self, request, pk):
+        task = Task.objects.get(id=pk)
+        context = {"task":task}
+
+        return render(request, "todo/task.html", context)
+
+
+    def post(self, request, pk):
+        task = Task.objects.get(id=pk)
+        task.body = request.POST.get("body")
+        task.save()
+
+        return redirect("tasks")
+
+
+
+class DeleteTask(View):
+    """
+      Delete a task
+    """
+    def get(self, request, pk):
+        task  = Task.objects.get(id=pk)
+        context = {"task": task}
+
+        return render(request, "todo/delete.html", context)
+
+    def post(self, request, pk):
+        task = Task.objects.get(id=pk)
+        task.delete()
+
+        return redirect("tasks")
